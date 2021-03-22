2459b7f09582caaa0eaf722dc7c8ab6085922c79
badrandom <denis.varihanov@ya.ru>
2021-03-22 23:05:59 +0300

    Lesson 1

--- quadratic.rb
+++ quadratic.rb
@@ -11,8 +11,10 @@ d = b**2 - 4 * a * c
 if d < 0
   print "No roots! D = #{d}"
 elsif d == 0
+  print "D = #{d}\n"
   print "x = #{(b * -1) / (2 * a).to_f}"
 else
+  print "D = #{d}\n"
   print "x1 = #{((b * -1) + Math.sqrt(d).to_f) / 2 / a}\n"
   print "x2 = #{((b * -1) - Math.sqrt(d).to_f) / 2 / a}\n"
 end
